243b10c34e226babb092046f65456c2668e46947
Alexandre de Freitas Souza <alexandre.souza@pixforce.ai>
2023-11-08 20:50:12 -0300

    feat:multiplying each digit by 1 and 3 alternately.

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,20 @@
 =begin
-The idea of this code is to receive an incomplete ISBN-13 code that has no check digit and return it complete
+The idea of this code is to receive an incomplete 
+ISBN-13 code that has no check digit and return it 
+complete
 =end
+
+puts "Please insert a ISBN-13 without a check digit"
+code = gets.chomp
+
+multiplier = 1
+multiplied_list = []
+
+code.each_char do |digit|
+    number = digit.to_i
+    multiplied_list.append(number * multiplier)
+    # multiplier must switch between 1 and 3
+    multiplier = 4 - multiplier
+end
+
+puts multiplied_list
