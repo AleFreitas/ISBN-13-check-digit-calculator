a301f833d1245bded0eb5414b1b9704617aa27a2
Alexandre de Freitas Souza <alexandre.souza@pixforce.ai>
2023-11-29 12:01:59 -0300

    test: input is string test implemented

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -3,7 +3,11 @@ require 'rspec'
 
 
 describe 'get_full_isbn13' do
-  it 'returns a full ISBN 13 code' do
+  it 'returns the complete ISBN-13' do
     expect(get_full_isbn13('978014300723')).to eq('9780143007234')
+  end
+  it 'raises an ArgumentError if the argument is not a string' do
+    expect { get_full_isbn13(123) }.to raise_error(ArgumentError, 'the argument must be a string')
+  
   end
 end
